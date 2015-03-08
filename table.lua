27179941d8e460a6ca951df5b2a1b9ef04fefbc4
oshiro-kazuma <oshiro_kazuma@ca-adv.co.jp>
2015-03-08 16:37:54 +0900

    List,Mapを使ってみる

---
+++ table.lua
@@ -0,0 +1,14 @@
+-- List
+local list = {1, 2, 3}
+print(list[1])
+print(list[2])
+print(list[3])
+
+-- Map
+local map = {}
+map.first = "shibuya"
+map.last = "tarou"
+map.age = 26
+print(map.first)
+print(map.last)
+print(map.age)
